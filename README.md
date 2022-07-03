1b6039a430f0bc2f83cae1a1196963d48f22ee9d
Lluis Sanchez <lluis.lsm@gmail.com>
2022-07-03 17:20:14 +0200

    feat: user/org and token parameters

--- README.md
+++ README.md
@@ -10,16 +10,24 @@ Update all the git repositories in a folder by typing `update_repos`.
 
 1. Install [GitHub CLI](https://cli.github.com) (necessary for `clone_repos`)
 2. Add the functions to your shell configuration file (e.g., `~/.zshrc` for zsh shell) by either:
-    1. Copying the desired functions from `bulk-utils.sh` directly
-    2. Importing it by adding: `source /path/to/git-utils/bulk-utils.sh`
+   1. Copying the desired functions from `bulk-utils.sh` directly
+   2. Importing it by adding: `source /path/to/git-utils/bulk-utils.sh`
 3. Reload the shell settings by typing `source ~/.zshrc` or close and reopen the terminal
 
 ## Usage
 
-To clone all repositories for a given user, navigate to the desired folder and type:
+### Clone all repositories for a giver user/org
 
-`clone_repos`
+Navigate to the desired folder and type:
 
-To update all git repositories in a folder, navigate to the desired folder and type:
+`clone_repos <user> <access_token>`
+
+When an access_token is provided, it will automatically download all the repos in a fully unattended mode.
+
+When an access_token is NOT provided, it will prompt the user to authenticate to GitHub and will assume that SSH is the preferred method for cloning the repos.
+
+### Udate all git repositories in a folder
+
+Navigate to the desired folder and type:
 
 `update_repos`
--- bulk-utils.sh
+++ bulk-utils.sh
@@ -1,34 +1,46 @@
 # Clones all git repos of a given user or organization using GitHub CLI (https://cli.github.com)
+# $1 user/org
+# $2 access_token (optional)
 clone_repos() {
-  # Log in and configure ssh for git operations
-  gh auth login -p ssh
 
-  # Get user or organization for which we will fetch all repos
-  echo 'For which user or organization do you wish to clone all repos: '
-  read -r user_or_org
+  if [ -z "$1" ]; then
+      echo "Please provide the user or organization for which to download the repos as a first parameter"
+      return
+  fi
 
-  incl_archived=0
-  while true; do
-      echo "Do you want to include archived repos? (Y/n): "
-      read -r yn
-      case $yn in
-          [Yy]* ) incl_archived=1; break;;
-          [Nn]* ) break;;
-          * ) printf "Please answer yes or no\n";;
-      esac
-  done
+  local user_or_org="${1}"
+  local include_archived=1
 
-  # Create a tmp file with all the repositories for that user/org
-  gh repo list "$user_or_org" $( (( incl_archived==0 )) && printf %s '--no-archived') | awk NF=1 > repos
+  # Login to GitHub
+  if [ -z "$2" ]; then
+    # Log in and configure ssh for git operations
+    gh auth login --hostname github.com -p ssh
+
+    # Ask whether to include archived repos
+    while true; do
+        echo "Do you want to include archived repos? (Y/n): "
+        read -r yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) include_archived=0; break;;
+            * ) printf "Please answer yes or no\n";;
+        esac
+    done
+  else 
+    # Log in using provided token
+    gh auth login --hostname github.com --with-token "${2}"
+  fi
+
+  # Create a temporary file with all the repositories for that user/org
+  gh repo list "$user_or_org" $( (( incl_archived==0 )) && printf %s '--no-archived') | awk NF=1 > repos.txt
 
   # Clone all the repositories
   while read p; do
     echo "Attempting to clone $p"
     gh repo clone $p
-  done <repos
+  done <repos.txt
 
-  # Delete the tmp file
-  rm repos
+  rm repos.txt
 }
 
 # Pulls all branches from remote for all the git repositories in the current folder
